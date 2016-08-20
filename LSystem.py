0f893377d21932b19f186d89b29c5158886c5f0b
Zach Waltman <z.waltman@gmail.com>
2016-08-19 20:27:57 -0700

    Slight modifications to system attribute modifiers

--- LSystem.py
+++ LSystem.py
@@ -5,33 +5,44 @@ Performs basic functions like modifying attributes of LSystem object,
 as well as generating the state of an LSystem at a specific iteration.
 """
 
+# Zach Waltman 
+# z.waltman@gmail.com
+
 class LSystem:
     """Create, modify, generate states of L-System"""
     def __init__(self):
         self.variables = []
         self.constants = []
         self.rules = {}
-        self.startState = ""
+        self.start = ""
 
     # SYSTEM VARIABLES
     def addVar(self, var):
-        self.variables.append(var)
+        if var not in self.variables:
+            self.variables.append(var)
 
     def getVars(self):
         return self.variables
 
-    def deleteVar(self, index):
-        del self.variables[index] 
+    def deleteVar(self, index=None):
+        if index == None:
+            self.variables = []
+        else:
+            del self.variables[index] 
 
     # SYSTEM CONSTANTS
     def addConst(self, const):
-        self.constants.append(const)
+        if const not in self.constants:
+            self.constants.append(const)
 
     def getConsts(self):
         return self.constants
 
-    def deleteConst(self, index):
-        del self.constants[index]
+    def deleteConst(self, index=None):
+        if index == None:
+            self.constants = []
+        else:
+            del self.constants[index]
 
     # SYSTEM RULES
     def addRule(self, a, b):
@@ -40,18 +51,21 @@ class LSystem:
     def getRules(self):
         return self.rules
 
-    def deleteRule(self, char):
-        del self.rules[char]
+    def deleteRule(self, char=None):
+        if char == None:
+            self.rules = {}
+        else:
+            del self.rules[char]
 
     # SYSTEM START STATE
-    def setStartState(self, startState):
-        self.startState = startState
+    def addStart(self, start):
+        self.start = start
 
-    def getStartState(self):
-        return self.startState
+    def getStart(self):
+        return self.start
 
-    def deleteStartState(self):
-        self.startState = ""
+    def deleteStart(self):
+        self.start = ""
 
     # OPERATIONS ON SYSTEM
     def applyRule(self, char):
@@ -61,7 +75,7 @@ class LSystem:
         return ''.join([self.applyRule(ch) for ch in state])
 
     def getState(self, num):
-        state = self.startState
+        state = self.start
         for i in range(num):
             state = self.iterate(state)
 
